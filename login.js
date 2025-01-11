bbbc1eb0c9c9ad9c425850862871ad2570e24a22
MA-NDIEP <mandiepayuk4@gmail.com>
2025-01-11 15:46:58 +1400

    connecting login page to the course page

--- login.js
+++ login.js
@@ -11,3 +11,26 @@
             password.type = "password"
               }
             }
+
+    document.getElementById("loginform").addEventListener("submit", function(event){
+      event.preventDefault();
+      var username = document.getElementById("Username").value;
+
+
+      var password = document.getElementById("password").value;
+
+      var user = localStorage.getItem(username);
+
+      if(user){
+        var parsedUser = JSON.parse(user);
+        if(parsedUser.password === password){
+          localStorage.setItem("user", JSON.stringify(parsedUser));
+          window.location.href = "./course.html";
+        }else{
+          alert("Incorrect pasword");
+        }
+      }else{
+        alert("user not found");
+      }
+
+    });
