bd81f28ab0b28c4333d1f9d56c8e3a68afeb3ab7
Takam-Ismael <ismaelulrich121@gmail.com>
2025-01-11 16:49:07 +0100

    adjust1

--- Admin/js/Navbar.js
+++ Admin/js/Navbar.js
@@ -1,3 +1,11 @@
 function Acc(){
-    window.location.href="../../html/Account.html"
+    window.location.href="Account.html"
+}
+
+function Req(){
+    window.location.href="Request.html"
+}
+
+function core(){
+    window.location.href="Admin.html"
 }
